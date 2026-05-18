64b2ed3025093bd06be8367d3ad552a0997e949e
Ashlesha gaikwad <ashleshagaikwad132@gmail.com>
2026-05-18 16:28:43 +0530

    AbsoluteValue.java

--- AbsoluteValue.java
+++ AbsoluteValue.java
@@ -1,13 +1,13 @@
-package Core;
-
 import java.util.Scanner;
 
 public class AbsoluteValue {
-    static void main() {
-        System.out.println("Enter the value:");
-        Scanner demo = new Scanner (System.in);
-        int n = demo.nextInt();
-        if (n< 0) n = -n;
-        System.out.println(n);
-    }
+public static void main(String[] args) {
+     System.out.println("Enter the value:");
+                Scanner demo = new Scanner (System.in);
+                int n = demo.nextInt();
+                if (n< 0) n = -n;
+                System.out.println(n);
+    
+}
+    
 }
