056a4974b42bc55640b7561f411abc7fcc1323fe
Ashlesha gaikwad <ashleshagaikwad132@gmail.com>
2026-05-20 10:09:12 +0530

    PermutationAndCombination.java

--- IsInteger.java
+++ IsInteger.java
@@ -1,9 +1,9 @@
 package Core;
-import.java.util.Scanner;
+import java.util.Scanner;
 public class IsInteger{
-    public static void main(){
+    public static void main(String[] args){
         System.out.println("Enter Number:");
-        Scanner sb  = new Scanner (System.in);
+        Scanner sb = new Scanner(System.in);
         double x = sb.nextDouble();
         int n = (int)x;
         if (x-n>0) System.out.println("is not integer");
