08ede0ad44d04f6565fda4aea05210da694cd86c
Ashlesha gaikwad <ashleshagaikwad132@gmail.com>
2026-05-20 10:40:41 +0530

    Add ReturnType.java

---
+++ ReturnType.java
@@ -0,0 +1,15 @@
+public class ReturnType {
+    public static int Ashu(int a) {
+        System.out.println("banu");
+        if (a>=0)return 5;
+        else return 10;
+    }
+        public static void main(){
+            int x =Ashu(7);
+            System.out.println(3+x);
+        }
+    }
+
+
+    
+
